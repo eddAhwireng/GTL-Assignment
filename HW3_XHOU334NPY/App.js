7fd42b6da44499bf903d10cc1ea1d4e0eb8da9b6
EDWIN AHWIRENG <68541578+eddAhwireng@users.noreply.github.com>
2020-08-14 08:09:43 +0100

    Add files via upload

---
+++ HW3_XHOU334NPY/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Eddie from './work.js';
+
+import './css/components.css';
+import './css/global.css';
+import './css/layout.css';
+import './css/sample-wallpaper.css';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Eddie/>
+      
+    </div>
+  );
+}
+
+export default App;
